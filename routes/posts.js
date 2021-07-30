1bc21351fb1db10d2cbb6d99d45b7003d4269fc2
hai061898 <truonghaiauto98@gmail.com>
2021-07-30 16:29:23 +0700

    #7

--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const postscontroller = require('../controllers/post.controller');
+const checkAuthmiddleware = require("../middleware/check_auth")
 
 const router = express.Router();
 
-router.get("/", postscontroller.index);
-router.post("/",postscontroller.save);
+router.get("/",postscontroller.index);
+router.post("/",checkAuthmiddleware.checkAuth ,postscontroller.save);
 router.get("/:id",postscontroller.show);
-router.patch("/:id",postscontroller.update);
-router.delete("/:id",postscontroller.destroy);
+router.patch("/:id",checkAuthmiddleware.checkAuth ,postscontroller.update);
+router.delete("/:id",checkAuthmiddleware.checkAuth ,postscontroller.destroy);
 
 module.exports = router;
